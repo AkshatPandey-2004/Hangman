409a506ff8025be032f82243f665a485ed4ccbda
nikhilQs <144718970+nikhilQs@users.noreply.github.com>
2023-09-29 14:32:46 +0530

    Added Buttons

--- Hangman.py
+++ Hangman.py
@@ -5,7 +5,25 @@ pygame.init()
 WIDTH, HEIGHT=800, 500
 win=pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Hangman Game")
- 
+
+
+#BUTTONS VARIABLE
+RADIUS=20
+GAP=15
+letters=[]
+startx=round((WIDTH-(RADIUS*2+ GAP)*13)/2)
+starty=400
+A=65
+for i in range(26):
+    x=startx +GAP * 2 + ((RADIUS*2+GAP) * (i%13)) 
+    y=starty+ ((i//13)) * (GAP + RADIUS * 2)
+    letters.append([x,y,chr(A+i),True])
+
+#Fonts
+LETTER_FONT=pygame.font.SysFont('comicsans',20)
+WORD_FONT=pygame.font.SysFont('comicsans',40)
+
+
 #load images
 images=[]
 for i in range(7):
@@ -15,6 +33,8 @@ for i in range(7):
 
 #colour
 WHITE=(255,255,255)
+BLACK=(0,0,0)
+
 #game variable
 hangman_status=0
 
@@ -22,13 +42,23 @@ hangman_status=0
 FPS=60
 clock=pygame.time.Clock()
 run=True
-while run:
-    clock.tick(FPS)
 
+def draw():
     win.fill(WHITE)
+    #DRAW BUTTONS
+    for letter in letters:
+        x, y, ltr, visible=letter
+        if visible:
+            pygame.draw.circle(win,BLACK,(x,y),RADIUS,3)
+            text=LETTER_FONT.render(ltr,1,BLACK)
+            win.blit(text,(x-text.get_width()/2,y-text.get_height()/2))
+
     win.blit(images[hangman_status],(150,100))
     pygame.display.update()
 
+while run:
+    clock.tick(FPS)
+    draw()
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             run=False
